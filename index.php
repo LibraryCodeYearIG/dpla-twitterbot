8b7afb090045125e63467ab186cf17e01b802740
Simon Hieu Mai <hieu.mai@mnsu.edu>
2014-01-24 12:20:44 -0500

    Test

---
+++ index.php
@@ -0,0 +1,14 @@
+<meta http-equiv="refresh" content="60" >
+
+<?php
+	echo "<p><font color=blue>Twitter BOT was activated.</font></p>";
+	
+	// grab new data from Twitter API #askDPLA	
+	
+	// format data --> query + identify the person have asked
+	
+	// query to DPLA
+	
+	// Respond to the person have asked
+
+?>
